04c8b9023317857562b40d65d669351a84dd31ce
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-12 16:41:46 -0600

    add new items

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function addNewItem() {
-    let newItem = document.getElementById("input").value
-    let li = document.createElement("li")
-    let ul = document.getElementById("list")
-    li.appendChild(document.createTextNode(newItem))
-    ul.appendChild(li)
-}
+document.getElementById("input").addEventListener("keypress", (e) => {
+    if (e.keyCode == 13) {
+        let ul = document.getElementById("list")
+        let li = document.createElement("li")
+        ul.appendChild(li)
+
+        let newItem = document.getElementById('input').value
+        li.appendChild(document.createTextNode(newItem))
+
+        document.getElementById('input').value = ""
+    }
+})
