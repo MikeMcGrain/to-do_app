8f153f8ef0280edbdb96e93beb3dae4710a9fe4a
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-12 23:58:24 -0600

    clear button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// add new list item
 document.getElementById("input").addEventListener("keypress", (e) => {
     if (e.keyCode == 13) {
         let ol = document.getElementById("list")
@@ -6,10 +7,15 @@ document.getElementById("input").addEventListener("keypress", (e) => {
 
         let newItem = document.getElementById("input").value
         li.appendChild(document.createTextNode(newItem))
-        
+
         li.addEventListener("click", function() {
             li.style.textDecorationLine = (li.style.textDecorationLine == "" || li.style.textDecorationLine == "none") ? "line-through":"none"
-        }) 
+        })
         input.value = ""
     }
+})
+
+// clear list
+document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("list").innerHTML = ""
 })
