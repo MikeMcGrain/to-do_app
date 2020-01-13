2cfbdbad58a54a2ac41fd472bbcd9b263734d4a9
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-13 11:40:12 -0600

    added comments to JS

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-// add new list item
 let inputElement = document.getElementById("input")
+
+// add new item to list
 inputElement.addEventListener("keypress", (e) => {
     if (e.keyCode == 13 && inputElement.value !== "") {
+        // create item element and append to list element
         let ol = document.getElementById("list")
         let li = document.createElement("li")
         ol.appendChild(li)
+
+        // create text node and append to item element
         li.appendChild(document.createTextNode(inputElement.value))
+
+        // add/remove "line-through" style when clicked
         li.addEventListener("click", (e) => {
             if (li.style.textDecorationLine == "" || li.style.textDecorationLine == "none") {
                 li.style.textDecorationLine = "line-through"
                 let i = document.createElement("i")
                 li.appendChild(i)
                 i.className = "fa fa-trash"
+                
+                // delete item when clicked
                 i.addEventListener("click", () => {
                     li.remove()
                 })
             } else {
+                // remove "line-through" style, remove trash icon
                 li.style.textDecorationLine = "none"
                 li.removeChild(li.firstElementChild)
             }
         })
+        // clear input
         input.value = ""
     }
 })
