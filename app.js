0143361f4e24a29f82c17e211bd954fee6443c9a
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-12 22:19:11 -0600

    cross out items

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ document.getElementById("input").addEventListener("keypress", (e) => {
         let newItem = document.getElementById('input').value
         li.appendChild(document.createTextNode(newItem))
 
+        li.addEventListener("click", function() {
+            if (li.style.textDecorationLine) {
+                li.style.textDecorationLine = "none"
+            }
+            else {
+                li.style.textDecorationLine = "line-through"
+            }
+        }) 
+
         document.getElementById('input').value = ""
     }
 })
