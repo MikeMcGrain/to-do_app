2e91b503d10526979bb3d0b3146d3d98eafe7323
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-14 11:28:44 -0600

    can store new items

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
-//on.load check storage
+let items =[]
 
-//if !null parse string and store as storedItems object
-//create new listItem for each completeItem and incompleteItem
-
-// console.log(items)
-// let items = JSON.parse(localStorage.getItem("items"))
-
-// function updateStorage() {
-//     items.incompleteItems = inputElement.value
-//     localStorage.setItem("items")
-// }
-let inputElement = document.getElementById("input")
-
-function addItem() {
+// adds an item to the list
+function addItem(itemText) {
     let ol = document.getElementById("list")
     let li = document.createElement("li")
     ol.appendChild(li)
-    li.appendChild(document.createTextNode(inputElement.value))
+    li.appendChild(document.createTextNode(itemText))
     
     // listens to add/remove "line-through" style and trash icon
     li.addEventListener("click", (e) => {
         if (li.style.textDecorationLine == "" || li.style.textDecorationLine == "none") {
-            //add to completedItems, delet from incompleteItems
             li.style.textDecorationLine = "line-through"
             let i = document.createElement("i")
             li.appendChild(i)
             i.className = "fa fa-trash"
-                    
+
             // listens to delete item
             i.addEventListener("click", () => {
-                //delete item from storage
                 li.remove()
             })
+
+            let completedStatus = true
+            storeStatus(completedStatus)
         } else {
-            //add to incompleteItems, delete from completedItems
             li.style.textDecorationLine = "none"
             li.removeChild(li.firstElementChild)
+
+            let completedStatus = false
+            storeStatus(completedStatus)
         }
-    })
-            
-    // clears input
-    inputElement.value = ""
+    })            
+    document.getElementById("input").value = ""
 }
+
+// stores item text
+function storeItem(itemText) {
+    let newItem = {}
+    newItem.text = itemText
+    newItem.completed = false
+    items.push(newItem)
+    localStorage.setItem("items", JSON.stringify(items))
+}
+
+// stores item status
+function storeStatus(completedStatus) {
+    items.completed = completedStatus
+    localStorage.setItem("items", JSON.stringify(items))
+}
+
+// listens to add new item
+document.getElementById("input").addEventListener("keypress", (e) => {
+    if (e.keyCode == 13 && document.getElementById("input").value !== "") {
+        let itemText = document.getElementById("input").value
+        storeItem(itemText)
+        addItem(itemText)
+    }
+})
 
 // listens to clear list and local storage
 document.getElementById("button").addEventListener("click", () => {
     document.getElementById("list").innerHTML = ""
+    items = []
     localStorage.clear()
-})
-
-// listens to add new item
-inputElement.addEventListener("keypress", (e) => {
-    if (e.keyCode == 13 && inputElement.value !== "") {
-        //add to incompleteItems
-        localStorage.setItem('item', inputElement.value)
-        addItem()
-    }
 })
