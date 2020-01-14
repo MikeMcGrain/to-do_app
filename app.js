c7e85a332ec391484e02dc20588a6113c9ba5f3e
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-13 22:07:32 -0600

    isolated addItem function

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 let inputElement = document.getElementById("input")
 
-// add new item to list
-inputElement.addEventListener("keypress", (e) => {
-    if (e.keyCode == 13 && inputElement.value !== "") {
-        // create item element and append to list element
-        let ol = document.getElementById("list")
-        let li = document.createElement("li")
-        ol.appendChild(li)
-
-        // create text node and append to item element
-        li.appendChild(document.createTextNode(inputElement.value))
-
-        // add/remove "line-through" style when clicked
-        li.addEventListener("click", (e) => {
-            if (li.style.textDecorationLine == "" || li.style.textDecorationLine == "none") {
-                li.style.textDecorationLine = "line-through"
-                let i = document.createElement("i")
-                li.appendChild(i)
-                i.className = "fa fa-trash"
-                
-                // delete item when clicked
-                i.addEventListener("click", () => {
-                    li.remove()
-                })
-            } else {
-                // remove "line-through" style, remove trash icon
-                li.style.textDecorationLine = "none"
+function addItem() {
+    let ol = document.getElementById("list")
+    let li = document.createElement("li")
+    ol.appendChild(li)
+    li.appendChild(document.createTextNode(inputElement.value))
+    
+    // listens to add/remove "line-through" style and trash icon
+    li.addEventListener("click", (e) => {
+        if (li.style.textDecorationLine == "" || li.style.textDecorationLine == "none") {
+            li.style.textDecorationLine = "line-through"
+            let i = document.createElement("i")
+            li.appendChild(i)
+            i.className = "fa fa-trash"
+                    
+            // event listeneer to delete
+            i.addEventListener("click", () => {
+                li.remove()
+            })
+        } else {
+            li.style.textDecorationLine = "none"
                 li.removeChild(li.firstElementChild)
             }
         })
-        // clear input
-        input.value = ""
-    }
-})
+            
+    // clears input
+    inputElement.value = ""
+}
 
-// clear list
+// listens to clear list and local storage
 document.getElementById("button").addEventListener("click", () => {
     document.getElementById("list").innerHTML = ""
+})
+
+// listens to add new item
+inputElement.addEventListener("keypress", (e) => {
+    if (e.keyCode == 13 && inputElement.value !== "") {
+        addItem()
+    }
 })
