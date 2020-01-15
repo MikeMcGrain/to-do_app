de0acb4120b9a9f3a45bd8e05651e7632b5b16ee
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-15 16:01:53 -0600

    one console error when deleting

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 let items = JSON.parse(localStorage.getItem('items'))
-addStoredItems()
+window.addEventListener("load", addStoredItems)
+
+// listens for new input
+document.getElementById("input").addEventListener("keypress", (e)=> {
+    if (e.keyCode == 13 && document.getElementById("input").value !== "") {
+        const itemText = document.getElementById("input").value 
+        const newItem = {
+            text: itemText,
+            completed: false
+        }
+        items.push(newItem)
+        localStorage.setItem("items", JSON.stringify(items))
+        addItem(newItem.text, newItem.completed)
+    }
+})
+
+// listener clears list and storage
+document.getElementById("button").addEventListener("click", ()=> {
+    document.getElementById("input").value = ""
+    document.getElementById("list").innerHTML = ""
+    items = []
+    localStorage.clear()
+})
 
 // gets items from storage and adds items to list
 function addStoredItems () {
@@ -17,21 +39,21 @@ function addStoredItems () {
 // creates an item and adds it to the list
 function addItem(itemText, completedStatus) {
     // add item to DOM
-    let ol = document.getElementById("list")
-    let li = document.createElement("li")
+    const ol = document.getElementById("list")
+    const li = document.createElement("li")
     ol.appendChild(li)
     li.appendChild(document.createTextNode(itemText))
 
     // add style and trash icon
     if (completedStatus == true) {
         li.style.textDecorationLine = "line-through"
-        let i = document.createElement("i")
+        const i = document.createElement("i")
         li.appendChild(i)
         i.className = "fa fa-trash"
 
         // add listener to trash icon
         i.addEventListener("click", (e) => {
-            let targetText = e.target.parentNode.innerText
+            const targetText = e.target.parentNode.innerText
             deleteFromStorage(targetText)               
             li.remove()
         }) 
@@ -41,56 +63,41 @@ function addItem(itemText, completedStatus) {
     li.addEventListener("click", (e) => {
         if (completedStatus == false) {
             li.style.textDecorationLine = "line-through"
-            let i = document.createElement("i")
+            const i = document.createElement("i")
             li.appendChild(i)
             i.className = "fa fa-trash"
-
-            completedStatus = true
-            storeStatus(completedStatus, e.target.innerText)
+            storeStatus(e.target.innerText, completedStatus=true)
 
             // listens to delete item
             i.addEventListener("click", (e) => {
-                let targetText = e.target.parentNode.innerText
+                const targetText = e.target.parentNode.innerText
                 deleteFromStorage(targetText)               
                 li.remove()
             })
         } else {
             li.style.textDecorationLine = "none"
             li.removeChild(li.firstElementChild)
-            completedStatus = false
-            storeStatus(completedStatus, e.target.innerText)
+            storeStatus(e.target.innerText, completedStatus=false)
         } 
-        
     })
-
     document.getElementById("input").value = ""
 }
 
-// stores item text
-function storeItem(itemText) {
-    let newItem = {
-        text: itemText,
-        completed: false
-    }
-    items.push(newItem)
-    localStorage.setItem("items", JSON.stringify(items))
-}
-
 // stores item status
-function storeStatus(completedStatus, targetText) {
+function storeStatus(targetText, completedStatus) {
     let liArray = []
-    let liElements = document.querySelectorAll("#list li")
+    const liElements = document.querySelectorAll("#list li")
     for(let i = 0; i < liElements.length; i++){
         liArray.push(liElements[i].innerText)
     }
-        items[liArray.indexOf(targetText)].completed = completedStatus
-        localStorage.setItem("items", JSON.stringify(items))
+    items[liArray.indexOf(targetText)].completed = completedStatus
+    localStorage.setItem("items", JSON.stringify(items))
 }
 
 // deletes item from storage
 function deleteFromStorage(targetText) {
     let liArray = []
-    let liElements = document.querySelectorAll("#list li")
+    const liElements = document.querySelectorAll("#list li")
     for(let i = 0; i < liElements.length; i++){
         liArray.push(liElements[i].innerText)
     }
@@ -98,20 +105,3 @@ function deleteFromStorage(targetText) {
     items.splice(indexRemovalStart, 1)
     localStorage.setItem("items", JSON.stringify(items))
 }
-
-// listener for new input
-document.getElementById("input").addEventListener("keypress", (e)=> {
-    if (e.keyCode == 13 && document.getElementById("input").value !== "") {
-        let itemText = document.getElementById("input").value
-        let completedStatus = false
-        storeItem(itemText)
-        addItem(itemText, completedStatus)
-    }
-})
-
-// listener clears list and storage
-document.getElementById("button").addEventListener("click", ()=> {
-    document.getElementById("list").innerHTML = ""
-    items = []
-    localStorage.clear()
-})
