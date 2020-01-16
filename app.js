f57e3c203d00e822c39a966297b983a7f87d7b2b
Mike McGrain <mcgrain.mike@gmail.com>
2020-01-15 18:31:50 -0600

    trimmed addItem()

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let items = JSON.parse(localStorage.getItem('items'))
 window.addEventListener("load", addStoredItems)
 
 // listens for new input
-document.getElementById("input").addEventListener("keypress", (e)=> {
+document.getElementById("input").addEventListener("keypress", (e) => {
     if (e.keyCode == 13 && document.getElementById("input").value !== "") {
         const itemText = document.getElementById("input").value 
         const newItem = {
@@ -16,7 +16,7 @@ document.getElementById("input").addEventListener("keypress", (e)=> {
 })
 
 // listener clears list and storage
-document.getElementById("button").addEventListener("click", ()=> {
+document.getElementById("button").addEventListener("click", () => {
     document.getElementById("input").value = ""
     document.getElementById("list").innerHTML = ""
     items = []
@@ -38,20 +38,22 @@ function addStoredItems () {
 
 // creates an item and adds it to the list
 function addItem(itemText, completedStatus) {
-    // add item to DOM
+    // add li and text to DOM
     const ol = document.getElementById("list")
     const li = document.createElement("li")
     ol.appendChild(li)
     li.appendChild(document.createTextNode(itemText))
 
-    // add style and trash icon
     if (completedStatus == true) {
+        // style li
         li.style.textDecorationLine = "line-through"
+
+        // add trash icon
         const i = document.createElement("i")
         li.appendChild(i)
         i.className = "fa fa-trash"
 
-        // add listener to trash icon
+        // add click listener to trash icon
         i.addEventListener("click", (e) => {
             const targetText = e.target.parentNode.innerText
             deleteFromStorage(targetText)               
@@ -59,26 +61,31 @@ function addItem(itemText, completedStatus) {
         }) 
     }
 
-    // Add listeners
+    // add click listener to li
     li.addEventListener("click", (e) => {
-        if (completedStatus == false) {
+        if (completedStatus == true) {
+            // remove style
+            li.style.textDecorationLine = "none"
+            // remove icon
+            li.removeChild(li.firstElementChild)
+            // change and store status
+            storeStatus(e.target.innerText, completedStatus=false)
+        } else {
+            // style li
             li.style.textDecorationLine = "line-through"
+            // add trash icon
             const i = document.createElement("i")
             li.appendChild(i)
             i.className = "fa fa-trash"
+            // change and store status
             storeStatus(e.target.innerText, completedStatus=true)
-
-            // listens to delete item
+            // add click listener to trash icon
             i.addEventListener("click", (e) => {
                 const targetText = e.target.parentNode.innerText
                 deleteFromStorage(targetText)               
                 li.remove()
-            })
-        } else {
-            li.style.textDecorationLine = "none"
-            li.removeChild(li.firstElementChild)
-            storeStatus(e.target.innerText, completedStatus=false)
-        } 
+             }) 
+        }
     })
     document.getElementById("input").value = ""
 }
